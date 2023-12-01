094232b3bc2b60df3369ff598232ca5d06e99e0e
siriroh kittiwuttikul <s55160304@gmail.com>
2023-12-01 14:00:51 +0700

    list finish - Addplayer - EditPlayer - ShowPlayer

--- SawadeeMonday/src/Player/Player.jsx
+++ SawadeeMonday/src/Player/Player.jsx
@@ -30,7 +30,7 @@ import Avatar from "@mui/material/Avatar";
 
 import "../Player/Player.css";
 import FetchData from "../Service/Service";
-import {Addprofile_service , Deleteprofile_service} from "../Player/Player_service";
+import {Addprofile_service , Deleteprofile_service, Editprofile_service, Getprofile_service} from "../Player/Player_service";
 import Allplayer_service from "../Service/Allplayer_service";
 import { Add, AllInboxSharp } from "@mui/icons-material";
 import { red } from "@mui/material/colors";
@@ -43,15 +43,13 @@ import 'sweetalert2/src/sweetalert2.scss';
 function Player() {
   const [open, Setopen] = useState(false);
 
-  const [Btn_checkprofile, setBtn_checkprofile] = useState(false);
-
   const [Avatar_pic, setAvatar_pic] = useState(null);
 
   const [Name_profile, setName_profile] = useState("");
 
   const [Phone_profile, setPhone_profile] = useState("");
 
-  const [Type_profile, setType_profile] = useState("");
+  const [Type_profile, setType_profile] = useState(0);
 
   const [Path_Profile, setPath_Profile] = useState(null);
 
@@ -62,16 +60,16 @@ function Player() {
   const [Alllistplayer, setAlllistplayer] = useState(null);
 
   const handleclose = () => {
+    setName_profile("");
+    setPhone_profile("");
+    setPath_Profile(null);
+    setType_profile(0);
     Setopen(false);
-    setAvatar_pic(null);
-    setName_profile(null);
-    setPhone_profile(null);
-    setType_profile(null);
-  };
+  };// กำหนดเวลาปิดปุ่ม Modal Addplayer
 
   const handleopen = () => {
     Setopen(true);
-  };
+  }; // กำหนดเวลาเปิดปุ่ม Modal Addplayer
 
   useEffect(() => {
     const fetch_type_player = async () => {
@@ -95,7 +93,7 @@ function Player() {
     }; //allplayer
     fetch_type_player();
     get_all_player();
-  }, []);
+  }, []); //เอาไว้เรียกค่า TypePlayer and Allplayer
 
   const ChangeTypePlayer = (event) => {
     setType_profile(event.target.value);
@@ -139,7 +137,7 @@ function Player() {
       </Grid>
 
       {/*function Allplayer*/}
-      <AllPlayer list={Alllistplayer}></AllPlayer>
+      <AllPlayer list={Alllistplayer} Typelist={Typelist}></AllPlayer>
       {/*เป็นตัวคุมการโชว์ Allplayer ทั้งหมด*/}
 
       {/*Modal*/}
@@ -169,9 +167,6 @@ function Player() {
                   sx={{ mt: 2 }}
                   className={"Modal_Avatar"}
                 >
-                  {/*<Avatar className={"Avatar"}>
-                    <AddAPhotoIcon></AddAPhotoIcon>
-                    </Avatar>*/}
                   <Grid item xs={12}>
                     <Hidden mdUp={false}>
                       <Button
@@ -235,10 +230,10 @@ function Player() {
                       fullWidth
                       className={"Text_field "}
                       color="primary"
-                      name="Name"
                       defaultValue=""
                       multiline={false}
                       item="true"
+                      value={Name_profile}
                       InputLabelProps={{
                         style: { color: "red" },
                       }}
@@ -279,10 +274,9 @@ function Player() {
                         className={"Grid_modal_select"}
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        
                         label="ประเภทมือ"
+                        value={Type_profile}
                         onChange={ChangeTypePlayer}
-                        defaultValue={0}
                       >
                         <MenuItem key={0} value={0}>
                           กรุณาเลือกประเภทฝีมือ
@@ -344,7 +338,51 @@ function Player() {
   );
 }
 
-const AllPlayer = ({ list = null }) => {
+const AllPlayer = ({ list = null ,Typelist = null}) => {
+
+  const [Edit_btn,setEditbtn] = useState(false);
+  const [EditName,setEditname] = useState("");
+  const [Editphone,setEditphone] = useState("");
+  const [EditPath,setEditpath] = useState(null);
+  const [EditType,setEditType] = useState(0);
+  const [EditAvatar,setEditAvatar] = useState(null);
+  const [EditPlayer_id,setEditPlayer_id] = useState(0);
+
+  const handleopenedit = (Player_id = null) =>{
+    Get_profile(Player_id);
+    setEditbtn(true);
+  }
+
+  const handlecloseedit = () =>{
+    setEditbtn(false);
+  }
+
+  const Editprofile = async() => {
+    try{
+      const res = await Editprofile_service(EditPath,EditName,Editphone,EditType,EditPlayer_id);
+      const result = await res;
+      if(result){
+        setEditbtn(false);
+      }
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  const Get_profile = async({Player_id=null}) =>{
+    try{
+      const res = await Getprofile_service(Player_id);
+      const result = await res;
+      setEditname(result[0].Player_name);
+      setEditpath(result[0].Player_photo);
+      setEditAvatar(result[0].Player_photo);
+      setEditphone(result[0].Player_tel)
+      setEditType(result[0].Type_id);
+      setEditPlayer_id(Player_id);
+    }catch(error){
+      console.error(error);
+    }
+  }
 
   const Delete_Btn = (Player_id) =>{
     //DeletePlayer(Player_id);
@@ -391,16 +429,210 @@ const AllPlayer = ({ list = null }) => {
                     <TableCell><span className={"Tablecell"}><Avatar className={"Tablecell_avatar"} alt="Remy Sharp" src={row.Player_photo} /><p>{row.Player_name}</p></span></TableCell>
                     <TableCell>{row.Type_id}</TableCell>
                     <TableCell>{row.Player_tel}</TableCell>
-                    <TableCell><span className={"Tablecell"}><EditIcon color={"primary"} className={"Tablecell_avatar"}></EditIcon><DeleteIcon color={"error"} onClick={() => Delete_Btn({Player_id:row.Player_id})}></DeleteIcon></span></TableCell>
+                    <TableCell><span className={"Tablecell"}><EditIcon onClick={() => handleopenedit({Player_id:row.Player_id})} color={"primary"} className={"Tablecell_avatar"}></EditIcon><DeleteIcon color={"error"} onClick={() => Delete_Btn({Player_id:row.Player_id})}></DeleteIcon></span></TableCell>
                   </TableRow>
       ))}
               </TableBody>
-            </Table>
+            </Table>  
           </TableContainer>
         </Grid>
       ) : (
         <p>ไม่มีข้อมูล</p>
       )}
+
+      <Modal
+        keepMounted
+        item="true"
+        open={Edit_btn}
+        onClose={handlecloseedit}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Fade in={Edit_btn} item={true}>
+          <Grid container>
+            <Box className={"Modal_Register_Player"}>
+              <Grid item xs={12}>
+                <Typography
+                  id="transition-modal-title"
+                  variant="h6"
+                  component="h2"
+                >
+                  รายละเอียดผู้เล่น
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography
+                  id="transition-modal-description"
+                  sx={{ mt: 2 }}
+                  className={"Modal_Avatar"}
+                >
+                  <Grid item xs={12}>
+                    <Hidden mdUp={false}>
+                      <Button
+                        id="Btn_Avatar"
+                        className={"Button_Avatar"}
+                        component="label"
+                        variant="contained"
+                        item="true"
+                        startIcon={
+                          EditAvatar === null ? (
+                            <AddAPhotoIcon className={"Avatar"}></AddAPhotoIcon>
+                          ) : (
+                            <Avatar
+                              className={"Profile_Avatar"}
+                              src={EditAvatar}
+                            ></Avatar>
+                          )
+                        }
+                      >
+                        <input
+                          type="file"
+                          id="Profile_Avatar"
+                          name="Profile_Avatar"
+                          accept="image/png, image/jpeg"
+                          multiline={false}
+                          onChange={(event) => {
+                            console.log(
+                              URL.createObjectURL(event.target.files[0])
+                            );
+                            setEditpath(
+                              event.target.files[0]
+                            );
+                            setEditAvatar(URL.createObjectURL(event.target.files[0]));
+                          }}
+                        ></input>
+                      </Button>
+                    </Hidden>
+                    <Hidden mdUp={true}>
+                      <Avatar src="#" className={"Button_Avatar"}>
+                        <input
+                          type="file"
+                          id="Profile_Avatar"
+                          name="Profile_Avatar"
+                          accept="image/png, image/jpeg"
+                          onChange={(event) => {
+                            console.log(event.target.value);
+
+                          }}
+                        ></input>
+                      </Avatar>
+                    </Hidden>
+                  </Grid>
+                </Typography>
+
+                <Grid item xs={12} className={"Grid_modal_text"}>
+                  <Box component={"form"} noValidate autoComplete="off">
+                    <TextField
+                      id="outlined-basic"
+                      label="ชื่อเว้ย"
+                      variant="outlined"
+                      fullWidth
+                      className={"Text_field "}
+                      color="primary"
+                      defaultValue=""
+                      multiline={false}
+                      item="true"
+                      value={EditName}
+                      InputLabelProps={{
+                        style: { color: "red" },
+                      }}
+                      onChange={(event) => {
+                        setEditname(event.target.value);
+                      }}
+                    ></TextField>
+                  </Box>
+                </Grid>
+                <Grid item xs={12}>
+                  <Box
+                    component={"form"}
+                    className={"Grid_modal_text"}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="เบอร์โทรเว้ย"
+                      variant="outlined"
+                      defaultValue=""
+                      multiline={false}
+                      value={Editphone}
+                      onChange={(event) => {
+                        setEditphone(event.target.value);
+                      }}
+                      fullWidth
+                    ></TextField>
+                  </Box>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl fullWidth className={"Grid_modal_select"}>
+                    <InputLabel id="demo-simple-select-label">
+                      ประเภทมือ
+                    </InputLabel>
+                    {Typelist ? (
+                      <Select
+                        className={"Grid_modal_select"}
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="ประเภทมือ"
+                        value={EditType}
+                      >
+                        <MenuItem key={0} value={0}>
+                          กรุณาเลือกประเภทฝีมือ
+                        </MenuItem>
+                        {Typelist.map((data) => (
+                          <MenuItem key={data.Type_id} value={data.Type_id}>
+                            {data.Type_name}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    ) : (
+                      <p>loading</p>
+                    )
+                    }
+                  </FormControl>
+                </Grid>
+              </Grid>
+              {/** Grid ใหญ่ */}
+              <Grid container spacing={2}>
+                <Grid xs={6}>
+                  {EditName && Editphone ? (
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      item="true"
+                      onClick={(event) =>{Editprofile()}}
+                    >
+                      Success 
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      item={true}
+                      disabled
+                    >
+                      Success
+                    </Button>
+                  )}
+                </Grid>
+                <Grid xs={6}>
+                  <Button
+                    variant="contained"
+                    color="warning"
+                    item="true"
+                    onClick={(event) => {
+                      handlecloseedit();
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                </Grid>
+              </Grid>
+              {/**Grid button */}
+            </Box>
+          </Grid>
+        </Fade>
+      </Modal>
     </>
   );
 };
--- SawadeeMonday/src/Player/Player_service.jsx
+++ SawadeeMonday/src/Player/Player_service.jsx
@@ -1,43 +1,68 @@
-import axios from "axios"
-import React,{useState} from "react"
+import axios from "axios";
+import React, { useState } from "react";
 
+export const Addprofile_service = async (Path, Name, Phone, Type) => {
+  const Formdata_newprofile = new FormData();
+  Formdata_newprofile.append("image", Path);
+  Formdata_newprofile.append("Name", Name);
+  Formdata_newprofile.append("Phone", Phone);
+  Formdata_newprofile.append("Type", Type);
+  try {
+    const res = await axios.post(
+      "http://127.0.0.1:7777/Add_player",
+      Formdata_newprofile,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
+    const result = await res.data;
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const Deleteprofile_service = async (Player_id) => {
+  const postdata = {
+    Player_id: Player_id,
+  };
+  try {
+    const res = await axios.post(
+      "http://127.0.0.1:7777/Deleteplayer",
+      postdata
+    );
+    const result = await res.data;
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const Addprofile_service = async(Path,Name,Phone,Type) => {
-    /*console.log(Path);
-    console.log(Name);
-    console.log(Phone);
-    console.log(Type);
-    const postdata ={
-        Photo:Path,
-        Name:  Name,
-        Phone: Phone,
-        Type:Type
-    }*/
+export const Getprofile_service = async (Player_id) => {
+  const postdata = {
+    Player_id: Player_id,
+  };
+  try {
+    const res = await axios.post("http://127.0.0.1:7777/Player", postdata);
+    const result = await res.data;
+    console.log(res);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    const Formdata_newprofile = new FormData();
-    Formdata_newprofile.append('image',Path);
-    Formdata_newprofile.append('Name',Name);
-    Formdata_newprofile.append('Phone',Phone);
-    Formdata_newprofile.append('Type',Type);
-    try{
-        const res = await axios.post("http://127.0.0.1:7777/Add_player",Formdata_newprofile,{headers : {'Content-Type':'multipart/form-data'}});
-        const result = await res.data;
-        return result;
-    }catch(error){
-        console.log(error);
-    }
-}
-
-export const Deleteprofile_service = async(Player_id) =>{
-    const postdata = {
-        'Player_id': Player_id
-    }
-    try{
-        const res = await axios.post("http://127.0.0.1:7777/Deleteplayer",postdata);
-        const result = await res.data;
-        return result;
-    }catch(error){
-        console.log(error);
-    }
-}
+export const Editprofile_service = async (Path, Name, Phone, Type,Player_id) => {
+ 
+  const Formdata_editprofile = new FormData();
+  Formdata_editprofile.append("image", Path);
+  Formdata_editprofile.append("Name", Name);
+  Formdata_editprofile.append("Phone", Phone);
+  Formdata_editprofile.append("Type", Type);
+  Formdata_editprofile.append("Player_id",Player_id);
+  try {
+    const res = await axios.post("http://127.0.0.1:7777/Editplayer",Formdata_editprofile,{ headers: { "Content-Type": "multipart/form-data" } });
+    const result = await res.data;
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
